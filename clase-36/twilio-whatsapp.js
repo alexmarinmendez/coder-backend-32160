fd8714d1d862ca365b3f638670989a238920314e
Alex Marin Mendez <alexmarinmendez@gmail.com>
2023-02-09 08:44:11 -0500

    feat: create a simple example sending a message template

--- clase-36/twilio-whatsapp.js
+++ clase-36/twilio-whatsapp.js
@@ -4,10 +4,10 @@ const authToken = process.env.TWILIO_AUTH_TOKEN;
 const client = require('twilio')(accountSid, authToken);
 
 client.messages
-      .create({
-         from: 'whatsapp:+14155238886',
-         body: 'Este es una prueba de envio luego del stop',
-         to: 'whatsapp:+51942270712'
-       })
-      .then(message => console.log(message.sid))
+      .create({ 
+         body: 'Your appointment is coming up on July 21 at 3PM', 
+         from: 'whatsapp:+14155238886',       
+         to: 'whatsapp:+51942270712' 
+       }) 
+      .then(message => console.log(message.sid)) 
       .catch(err => console.log(err));
